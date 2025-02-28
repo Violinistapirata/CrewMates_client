eba1fe8a5d551ba1fb308d1209d78df46355e168
Andrea Bronzoni <bronzoni.andrea@libero.it>
2025-02-28 21:37:00 +0100

    implemented useEffect to check if customer belongs to a group - working if customer is already in a group

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,8 +1,14 @@
 import "./DashboardPage.css";
-import { useState } from "react";
+import { useState, useContext, useEffect } from "react";
+import { AuthContext } from "../context/auth.context.jsx";
 const API_URL = import.meta.env.VITE_API_URL;
 
 function DashboardPage() {
+    const storedToken = localStorage.getItem("authToken");
+    const {userInfo} = useContext(AuthContext);
+    const [group, setGroup] = useState(null);
+    const [tasks, setTasks] = useState(null);
+    const [members, setMembers] = useState(null);
     const [groupCode, setGroupCode] = useState("");
     const [requestIsSent, setRequestIsSent] = useState(false)
     /* function handleOnChange(e) {
@@ -13,7 +19,7 @@ function DashboardPage() {
         e.preventDefault();
         console.log("Join group form submitted");
         setRequestIsSent(true);
-        fetch(`${API_URL}/group/${groupCode}`, {method: "PUT", headers: { 'Content-Type': 'application/json'}, body: JSON.stringify({members: userId})})
+        fetch(`${API_URL}/group/${groupCode}`, {method: "PUT", headers: { 'Content-Type': 'application/json'}, body: JSON.stringify({members: userInfo._id})})
         .then((response) => response.json())
         .catch((error) => {
             console.error("Error while updating the group ->", error);
@@ -21,10 +27,27 @@ function DashboardPage() {
         
         }
 
-     
-    
+/*     function findUserGroup() {
+        fetch(`${API_URL}/:id`, 
+    } */
+
+    useEffect(() => {
+        fetch(`${API_URL}/api/users/${userInfo._id}`, {method: "GET", headers:{ 'Content-Type': 'application/json', Authorization: `Bearer ${storedToken}` }})
+        .then((response) => {
+            console.log("response fetch ", response)
+            return response.json();
+        })
+        .then((data) => setGroup(data.group))
+        .catch((error) =>{
+            console.error("Error while checking the group ->", error);
+        })
+    }, [])
+
+    if(!group) { 
     return(
+        
         <>
+
             <h3> Join an existing group </h3>
             <p>If there is an existing group, ask one of the members for the group code. They can find it in the group settings.</p>
             <form onSubmit = {handleSubmit}>
@@ -41,5 +64,5 @@ function DashboardPage() {
         </>
     )
 }
-
+}
 export default DashboardPage;
