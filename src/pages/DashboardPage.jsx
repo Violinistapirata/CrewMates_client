f69e431102fe894532ff3c43c33bce1fd18c4ea3
Andrea Bronzoni <bronzoni.andrea@libero.it>
2025-03-01 19:47:27 +0100

    handled responses in fetch to a user in DashboardPage.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -6,19 +6,26 @@ import "../components/WeekTasks.jsx";
 
 import { useState, useContext, useEffect } from "react";
 import { AuthContext } from "../context/auth.context.jsx";
+import { Link } from "react-router-dom";
 import GroupAssignment from "../components/GroupAssignment.jsx";
 import GroupCreation from "../components/GroupCreation.jsx";
+import GroupMembers from "../components/GroupMembers.jsx";
+import WeekTasks from "../components/WeekTasks.jsx";
+
+import { getCurrentDate } from "../utils/helperFunctions.js";
 const API_URL = import.meta.env.VITE_API_URL;
 
 function DashboardPage() {
   const storedToken = localStorage.getItem("authToken");
   const { userInfo } = useContext(AuthContext);
   const [group, setGroup] = useState(null);
+  const [hasRecurringTasks, setHasRecurringTasks] = useState(false);
   const [tasks, setTasks] = useState(null);
+  const [hasActiveWeek, setHasActiveWeek] = useState(false);
   const [members, setMembers] = useState(null);
 
   useEffect(() => {
-    if (userInfo) {
+    userInfo && //with this we can recover the Context information. The Context is lost as soon as the page is refreshed.
       fetch(`${API_URL}/api/users/${userInfo._id}`, {
         method: "GET",
         headers: {
@@ -27,25 +34,83 @@ function DashboardPage() {
         },
       })
         .then((response) => {
-          console.log("response fetch ", response);
           return response.json();
         })
-        .then((data) => setGroup(data.group))
+        .then((userInfo) => {
+          setGroup(userInfo.group);
+          return userInfo.group;
+        })
+        .then((groupId) => {
+            console.log("groupId", groupId)
+        const currentDate = getCurrentDate();
+          {
+            groupId &&
+              fetch(`${API_URL}/api/week/${groupId}/${currentDate}`, {
+                method: "GET",
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: `Bearer ${storedToken}`,
+                },
+              })
+                .then((response) => {
+                  return response.json();
+                })
+                .then((weekStatus)=> {
+                    setHasRecurringTasks(weekStatus.hasRecurringTasks);
+                    setHasActiveWeek(weekStatus.hasActiveWeek);
+                    setTasks(weekStatus.tasks);
+                    console.log("data", weekStatus);
+                });
+
+          }
+        })
         .catch((error) => {
           console.error("Error while checking the group ->", error);
         });
-    }
   }, [userInfo]);
 
-  if (!group) {
-    return (
-      <>
-        <h1>Welcome to crewmates!</h1>
-        <p>Let's get started</p>
-        <GroupAssignment />
-        <GroupCreation />
-      </>
-    );
-  }
+  return (
+    <>
+      {!group && (
+        <>
+          <h1>Welcome to crewmates!</h1>
+          <p>Let's get started</p>
+          <GroupAssignment />
+          <GroupCreation />
+        </>
+      )}
+
+      {group && tasks && (
+        <>
+          <h1>Welcome on board</h1>
+          <GroupMembers />
+          <WeekTasks />
+        </>
+      )}
+
+      {group && !tasks && hasRecurringTasks && (
+        <>
+          <h1>Welcome on board!</h1>
+          <GroupMembers />
+          <button>Create new week</button>
+        </>
+      )}
+
+      {group && !tasks && !hasRecurringTasks && (
+        <>
+          <h1>Welcome on board!</h1>
+          <GroupMembers />
+          <p>
+            {" "}
+            Your group has no template to generate your tasks for the week.{" "}
+            <br />
+            Go to your{" "}
+            <Link to={`/settings/groups/${group}`}>Group Settings</Link> to add
+            tasks.{" "}
+          </p>
+        </>
+      )}
+    </>
+  );
 }
 export default DashboardPage;
