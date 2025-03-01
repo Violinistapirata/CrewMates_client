98c4e79366606406be5fa109d5864381921c0d54
Eleni Kriezia <173841123+nthTimeIsTheCharm@users.noreply.github.com>
2025-03-01 10:14:40 +0100

    Added main to App.jsx and corresponding styles in App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,43 @@
-import './App.css'
-import {Routes, Route} from 'react-router-dom';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 //pages
-import LandingPage from './pages/LandingPage';
-import SignUpPage from './pages/SignUpPage';
-import DashboardPage from './pages/DashboardPage';
-import UserSettingsPage from './pages/UserSettingsPage';
-import GroupSettingsPage from './pages/GroupSettingsPage';
-import NotFoundPage from './pages/NotFoundPage';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import AboutUsPage from './pages/AboutUsPage';
+import LandingPage from "./pages/LandingPage";
+import SignUpPage from "./pages/SignUpPage";
+import DashboardPage from "./pages/DashboardPage";
+import UserSettingsPage from "./pages/UserSettingsPage";
+import GroupSettingsPage from "./pages/GroupSettingsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import AboutUsPage from "./pages/AboutUsPage";
 
 function App() {
-
   return (
     <>
       <Navbar />
 
-      <Routes>
-        <Route 
-          path="/" 
-          element={<LandingPage/>} />
-        <Route 
-          path="/about-us" 
-          element={<AboutUsPage/>} />
-        <Route 
-          path="/sign-up" 
-          element={<SignUpPage/>} />
-        <Route 
-          path="/dashboard" 
-          element={<DashboardPage/>} />
-        <Route 
-          path="/settings/users/:userId" 
-          element={<UserSettingsPage/>} />
-        <Route 
-          path="/settings/groups/:groupId" 
-          element={<GroupSettingsPage/>} />
-        <Route 
-          path="*" 
-          element={<NotFoundPage/>} />  
-      </Routes>
+      <main>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/about-us" element={<AboutUsPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route
+            path="/settings/users/:userId"
+            element={<UserSettingsPage />}
+          />
+          <Route
+            path="/settings/groups/:groupId"
+            element={<GroupSettingsPage />}
+          />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
 
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
