0bae0cdb627d3cc3801c1fd810d1b27b13e3b868
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2025-03-01 23:48:28 +0100

    Created a variable to store a deep copy of the userGroupInfo object. Used the created variable as the initial value for formData. Created a state to store the deleted members from the group members list. Changed the condition in the ternary operator of the members section to check the members array length to be higher than 0. Added index parameter in members map method to pass it as props to the handleDelete function. Changed type of the delete button to type='button'. Created the handleDelete function to remove a member of the members array in the formData state and insert it in the deletedMembers array. Updated the cancel changes button to reset formData to the values of the userGroupInfoCopy and clean the deletedMembers array

--- src/components/UpdateGroupForm.jsx
+++ src/components/UpdateGroupForm.jsx
@@ -1,10 +1,20 @@
 import "./updateGroupForm.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
 function UpdateGroupForm({ setIsEditing, userGroupInfo, setUserGroupInfo }) {
-  const [formData, setFormData] = useState(userGroupInfo);
+  // In the formData state the initial value is set to be a copy of userGroupInfo and the arrays inside of it are set to be also copies of the original arrays to avoid modifying the userGroupInfo object and be able to recover it's content when discarding changes
+  const userGroupInfoCopy = {
+    ...userGroupInfo,
+    members: [...userGroupInfo.members],
+    recurringTasks: [...userGroupInfo.recurringTasks]
+  }
+  
+  const [formData, setFormData] = useState(userGroupInfoCopy);
+  console.log("THIS IS FORM DATA: ", formData);
+  const [deletedMembers, setDeletedMembers] = useState([]);
+  console.log("THIS IS THE DELETED MEMBERS ARRAY: ", deletedMembers);
 
   const [errorMessage, setErrorMessage] = useState(null);
   const [successMessage, setSuccessMessage] = useState(null);
@@ -15,15 +25,26 @@ function UpdateGroupForm({ setIsEditing, userGroupInfo, setUserGroupInfo }) {
 
   function handleSubmit(e) {
     e.preventDefault();
-    
-    // fetch(`${API_URL}/auth/login`, {
-        //   method: "POST",
-        //   headers: { "Content-Type": "application/json" },
-        //   body: JSON.stringify(formData),
-        // })
-        setUserGroupInfo(formData)
-        setIsEditing(false);
+
+    // fetch(`${API_URL}/api/groups/`, {
+    //   method: "PUT",
+    //   headers: { "Content-Type": "application/json" },
+    //   body: JSON.stringify(formData),
+    // })
+    setUserGroupInfo(formData);
+    // setDeletedMembers([])
+    setIsEditing(false);
   }
+
+  function handleDelete(index) {
+    // console.log("THIS IS THE DELETED MEMBER: ", formData.members[index]);
+
+    const deletedMember = formData.members.splice(index, 1);
+    // console.log("THIS IS deletedMember: ", deletedMember[0]);
+    // The splice() method returns an array with one object
+    setDeletedMembers([...deletedMembers, deletedMember[0]]);
+}
+
 
   return (
     <>
@@ -39,16 +60,24 @@ function UpdateGroupForm({ setIsEditing, userGroupInfo, setUserGroupInfo }) {
         <section className="section">
           <h3 className="section__title">My crewmates</h3>
           <ul className="section__list">
-            {formData.members ? (
-              formData.members.map((member) => {
+            {formData.members.length > 0 ? (
+              formData.members.map((member, index) => {
                 return (
                   <>
                     <li key={member._id} className="list-item">
-                    <div className="list-item__container">
-                      <div className="list-item__user-image">{member.name[0]}</div>
-                      <p className="list-item__name">{member.name}</p>
-                    </div>
-                    <button className="list-item__delete-btn">X</button>
+                      <div className="list-item__container">
+                        <div className="list-item__user-image">
+                          {member.name[0]}
+                        </div>
+                        <p className="list-item__name">{member.name}</p>
+                      </div>
+                      <button
+                        className="list-item__delete-btn"
+                        type="button"
+                        onClick={() => handleDelete(index)}
+                      >
+                        X
+                      </button>
                     </li>
                   </>
                 );
@@ -59,22 +88,31 @@ function UpdateGroupForm({ setIsEditing, userGroupInfo, setUserGroupInfo }) {
           </ul>
         </section>
 
-        <label htmlFor="recurringTasks">Tasks</label>
+        <label htmlFor="recurringTasks">Recurring tasks</label>
         <input
           type="text"
           name="recurringTasks"
           onChange={handleOnChange}
           value={formData.recurringTasks}
         />
-        <button type="button"
+        <button
+          type="button"
           className="form__button--cancel"
-          onClick={()=>setFormData(userGroupInfo)}
+          onClick={() => {
+            console.log(
+              "SET FORM DATA -->",
+              formData,
+              "USER GROUP INFO -->",
+              userGroupInfo
+            );
+            setFormData(userGroupInfoCopy);
+            setDeletedMembers([]);
+          }}
         >
           Cancel changes
         </button>
         <button className="form__button" type="submit">
-          {" "}
-          Update Group{" "}
+          Update Group
         </button>
         {errorMessage && <p className="error">❌ {errorMessage}</p>}
         {successMessage && <p className="success">✅ {successMessage}</p>}
