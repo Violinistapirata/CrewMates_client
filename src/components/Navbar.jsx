3f7b38c38ba314be072752330f545192f948e681
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2025-03-04 22:00:20 +0100

    Added icons for settings and dashboard to assets folder. Added the type and content properties in the login form button to make it work. Imported the useState hook in the Navbar component. Also imported the icons for the settings and dashboard tabs. Created state variables for showing the drop-down menu of setting pages options and for saving the page chosen by the user to activate the activeClassName parameter in the settings tab in both cases. Modified all NavLinks and created the NavLink for Settings with the drop-down menu incorporated.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,130 @@
 import "./Navbar.css";
 import { NavLink } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../context/auth.context";
 import homeIcon from "../assets/home-icon.png";
-import loginIcon from "../assets/login-icon.png"
-import logoutIcon from "../assets/logout-icon.png"
+import loginIcon from "../assets/login-icon.png";
+import logoutIcon from "../assets/logout-icon.png";
+import settingsIcon from "../assets/settings-icon.png";
+import dashboardIcon from "../assets/dashboard-icon.png";
 
 function Navbar() {
-    const {isLoggedIn, logOutUser} = useContext(AuthContext)
+  const { isLoggedIn, logOutUser } = useContext(AuthContext);
+  const [showSettingsDropDown, setShowSettingsDropDown] = useState(false);
+  const [chosenSettingsPage, setChosenSettingsPage] = useState("");
 
-    return(
-        <nav className="navbar">
-            <ul className = "navbar__links">
-                <li><NavLink to="/"><img src={homeIcon} alt="Home" className="nav-icon"/>Home</NavLink></li>
-                {
-                !isLoggedIn ? (<li><NavLink to="/sign-up"><img src={loginIcon} alt="Start Session" className="nav-icon" />Start Session</NavLink></li>) 
-                            : (<li><button onClick={logOutUser}><img src={logoutIcon} alt="Log Out" className="nav-icon" />Log Out</button></li>) //NavLink and handleLogout function?
-                }
-            </ul>
-        </nav>
-    )
+  return (
+    <nav className="navbar">
+      <ul className="navbar__links">
+        <li>
+          <NavLink
+            to="/"
+            activeClassName="active"
+            onClick={() => {
+              setShowSettingsDropDown(false);
+              setChosenSettingsPage("");
+            }}
+          >
+            <img src={homeIcon} alt="Home" className="nav-icon" />
+            Home
+          </NavLink>
+        </li>
+        {
+          !isLoggedIn ? (
+            <li>
+              <NavLink to="/sign-up" activeClassName="active">
+                <img src={loginIcon} alt="Start Session" className="nav-icon" />
+                Start Session
+              </NavLink>
+            </li>
+          ) : (
+            <>
+              <li>
+                <NavLink
+                  to="/dashboard"
+                  activeClassName="active"
+                  onClick={() => {
+                    setShowSettingsDropDown(false);
+                    setChosenSettingsPage("");
+                  }}
+                >
+                  <img
+                    src={dashboardIcon}
+                    alt="Dashboard"
+                    className="nav-icon"
+                  />
+                  Dashboard
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/sign-up"
+                  activeClassName="active"
+                  onClick={() => {
+                    logOutUser;
+                    setChosenSettingsPage("");
+                    setShowSettingsDropDown(false);
+                  }}
+                >
+                  <img src={logoutIcon} alt="Log Out" className="nav-icon" />
+                  Log Out
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={
+                    chosenSettingsPage === "user"
+                      ? "/settings/users/aaa"
+                      : chosenSettingsPage === "group"
+                      ? "/settings/groups/aaa"
+                      : null
+                  }
+                  activeClassName="active"
+                  onClick={() => {
+                    setShowSettingsDropDown(!showSettingsDropDown);
+                    setChosenSettingsPage("");
+                  }}
+                >
+                  <img
+                    src={settingsIcon}
+                    alt="settings-icon"
+                    className="nav-icon nav-icon__drop-down"
+                  />
+                  Settings
+                </NavLink>
+                {showSettingsDropDown && (
+                  <div className="drop-down">
+                    <NavLink to="/settings/users/aaa" activeClassName="active">
+                      <button
+                        onClick={() => {
+                          setChosenSettingsPage("user");
+                          setShowSettingsDropDown(false);
+                        }}
+                        className="drop-down__button"
+                      >
+                        User Settings
+                      </button>
+                    </NavLink>
+                    <NavLink to="/settings/groups/aaa" activeClassName="active">
+                      <button
+                        onClick={() => {
+                          setChosenSettingsPage("group");
+                          setShowSettingsDropDown(false);
+                        }}
+                        className="drop-down__button"
+                      >
+                        Group Settings
+                      </button>
+                    </NavLink>
+                  </div>
+                )}
+              </li>
+            </>
+          ) //NavLink and handleLogout function?
+        }
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
