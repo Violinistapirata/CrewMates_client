a494e55ce52b6f83b204b2976f195b9efb796b94
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2025-03-05 01:28:02 +0100

    Added the keyword 'exact' to route paths in app.jsx. Added wavy underscore in active class for the active nav tabs. Created a component for the settings tab and imported it in Navbar.jsx. Imported userInfo from context to apply the user and group ids to the paths in the NavLinks. Configured the user Settings page to be the default page for Settings.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,16 @@ import "./Navbar.css";
 import { NavLink } from "react-router-dom";
 import { useContext, useState } from "react";
 import { AuthContext } from "../context/auth.context";
+import SettingsTab from "./SettingsTab";
 import homeIcon from "../assets/home-icon.png";
 import loginIcon from "../assets/login-icon.png";
 import logoutIcon from "../assets/logout-icon.png";
-import settingsIcon from "../assets/settings-icon.png";
 import dashboardIcon from "../assets/dashboard-icon.png";
 
 function Navbar() {
-  const { isLoggedIn, logOutUser } = useContext(AuthContext);
+  const { isLoggedIn, logOutUser, userInfo } = useContext(AuthContext);
   const [showSettingsDropDown, setShowSettingsDropDown] = useState(false);
-  const [chosenSettingsPage, setChosenSettingsPage] = useState("");
+  const [chosenSettingsPage, setChosenSettingsPage] = useState(null);
 
   return (
     <nav className="navbar">
@@ -22,7 +22,7 @@ function Navbar() {
             activeClassName="active"
             onClick={() => {
               setShowSettingsDropDown(false);
-              setChosenSettingsPage("");
+              setChosenSettingsPage("users");
             }}
           >
             <img src={homeIcon} alt="Home" className="nav-icon" />
@@ -45,7 +45,7 @@ function Navbar() {
                   activeClassName="active"
                   onClick={() => {
                     setShowSettingsDropDown(false);
-                    setChosenSettingsPage("");
+                    setChosenSettingsPage("users");
                   }}
                 >
                   <img
@@ -61,8 +61,8 @@ function Navbar() {
                   to="/sign-up"
                   activeClassName="active"
                   onClick={() => {
-                    logOutUser;
-                    setChosenSettingsPage("");
+                    logOutUser();
+                    setChosenSettingsPage("users");
                     setShowSettingsDropDown(false);
                   }}
                 >
@@ -70,57 +70,15 @@ function Navbar() {
                   Log Out
                 </NavLink>
               </li>
-              <li>
-                <NavLink
-                  to={
-                    chosenSettingsPage === "user"
-                      ? "/settings/users/aaa"
-                      : chosenSettingsPage === "group"
-                      ? "/settings/groups/aaa"
-                      : null
-                  }
-                  activeClassName="active"
-                  onClick={() => {
-                    setShowSettingsDropDown(!showSettingsDropDown);
-                    setChosenSettingsPage("");
-                  }}
-                >
-                  <img
-                    src={settingsIcon}
-                    alt="settings-icon"
-                    className="nav-icon nav-icon__drop-down"
-                  />
-                  Settings
-                </NavLink>
-                {showSettingsDropDown && (
-                  <div className="drop-down">
-                    <NavLink to="/settings/users/aaa" activeClassName="active">
-                      <button
-                        onClick={() => {
-                          setChosenSettingsPage("user");
-                          setShowSettingsDropDown(false);
-                        }}
-                        className="drop-down__button"
-                      >
-                        User Settings
-                      </button>
-                    </NavLink>
-                    <NavLink to="/settings/groups/aaa" activeClassName="active">
-                      <button
-                        onClick={() => {
-                          setChosenSettingsPage("group");
-                          setShowSettingsDropDown(false);
-                        }}
-                        className="drop-down__button"
-                      >
-                        Group Settings
-                      </button>
-                    </NavLink>
-                  </div>
-                )}
-              </li>
+                <SettingsTab 
+                chosenSettingsPage={chosenSettingsPage} 
+                setChosenSettingsPage={setChosenSettingsPage} 
+                showSettingsDropDown={showSettingsDropDown} 
+                setShowSettingsDropDown={setShowSettingsDropDown}
+                userInfo={userInfo}
+                />
             </>
-          ) //NavLink and handleLogout function?
+          )
         }
       </ul>
     </nav>
