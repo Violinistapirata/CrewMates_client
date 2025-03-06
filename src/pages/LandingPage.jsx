b29d2b2d4a158b7c12235b264936b3918048925b
Airam86 <satodi86@gmail.com>
2025-03-06 21:07:42 +0100

    landing page css in process

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,28 @@
 import "./LandingPage.css";
 
 function LandingPage() {
-    return(
-        <>
-            <h2>Landing Page</h2>
-        </>
-    )
+  return (
+    <div className="landing-page-container">
+      <h2 className="landing-page-title">Bienvenidos a Crewmates!</h2>
+      <p className="landing-page-text">
+        "Ahoy, matey! Ready to join CrewMates, the ultimate treasure for taming
+        household chores? Arrr, no need to walk the plank here! With CrewMates,
+        signing up is quicker than a parrot swiping doubloons 🦜💰. Assemble yer
+        crew with those scallywags sharing yer deck (be they family or
+        flatmates) and mark 'em like treasures on yer map! Assign chores like a
+        fair captain! No one'll escape scrubbin' dishes 🍽️⚔️ or wringin' rags...
+        or they'll face the Responsibility Kraken! Organize tasks with lists
+        shinier than a chest overflowing with doubloons, and make sure every
+        scallywag pulls their weight... or ye'll make 'em walk the plank
+        (metaphorically, aye)! With CrewMates, chores become a crew feast, not a
+        mutiny! Yarrr, cleaning's never been this fun... or at least less
+        hateful! 🏴☠️✨ Weigh anchor and sail toward a chaos-free home, or I'll
+        make ye dance the hornpipe! Beware! If ye neglect it, dust'll pile
+        high... and CrewMate's guardian parrot will come for yer dirty socks.
+        🦜🧦🔥"
+      </p>
+    </div>
+  );
 }
 
 export default LandingPage;
