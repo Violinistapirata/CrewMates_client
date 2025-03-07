81fda8e48cc9e8c56dde32ef1e2f8b53db0d2cf8
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2025-03-07 01:09:02 +0100

    Added basic layout to landing page mobile version

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,28 +2,20 @@ import "./LandingPage.css";
 
 function LandingPage() {
     return(
-        <>
+        <div className="landing-page__container">
             <h2 className="landing-page__title">Welcome to Crewmates!</h2>
-            <div className="landing-page__ship-container">
-            {/* <img className="landing-page__pirate-ship" src="src/assets/pirate-ship-and-sea.png" alt="pirate-ship-on-the-sea" /> */}
-            {/* <img className="landing-page__sea" src="src/assets/sea-background.png" alt="pirate-ship-on-the-sea" /> */}
-            <p >"Ahoy, matey! Ready to join CrewMates, the ultimate treasure for taming
-        household chores? Arrr, no need to walk the plank here! With CrewMates,
-        signing up is quicker than a parrot swiping doubloons :loro::bolsa_de_dinero:. Assemble yer
-        crew with those scallywags sharing yer deck (be they family or
-        flatmates) and mark 'em like treasures on yer map! Assign chores like a
-        fair captain! No one'll escape scrubbin' dishes :cuchillo_tenedor_plato::espadas_cruzadas: or wringin' rags...
-        or they'll face the Responsibility Kraken! Organize tasks with lists
-        shinier than a chest overflowing with doubloons, and make sure every
-        scallywag pulls their weight... or ye'll make 'em walk the plank
-        (metaphorically, aye)! With CrewMates, chores become a crew feast, not a
-        mutiny! Yarrr, cleaning's never been this fun... or at least less
-        hateful! :ondeando_bandera_negra::calavera_y_tibias_cruzadas::destellos: Weigh anchor and sail toward a chaos-free home, or I'll
-        make ye dance the hornpipe! Beware! If ye neglect it, dust'll pile
-        high... and CrewMate's guardian parrot will come for yer dirty socks.
-        :loro::calcetines::fuego:"</p>
-            </div>
-        </>
+            <p className="landing-page__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure odio sit, quod iusto ducimus libero reprehenderit nulla dignissimos necessitatibus quisquam esse minima sint aspernatur modi tenetur voluptate pariatur illo suscipit!</p>
+            <p className="landing-page__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure odio sit, quod iusto ducimus libero reprehenderit nulla dignissimos necessitatibus quisquam esse minima sint aspernatur modi tenetur voluptate pariatur illo suscipit!</p>
+            <p className="landing-page__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure odio sit, quod iusto ducimus libero reprehenderit nulla dignissimos necessitatibus quisquam esse minima sint aspernatur modi tenetur voluptate pariatur illo suscipit!</p>
+            <p className="landing-page__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure odio sit, quod iusto ducimus libero reprehenderit nulla dignissimos necessitatibus quisquam esse minima sint aspernatur modi tenetur voluptate pariatur illo suscipit!</p>
+            <p className="landing-page__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure odio sit, quod iusto ducimus libero reprehenderit nulla dignissimos necessitatibus quisquam esse minima sint aspernatur modi tenetur voluptate pariatur illo suscipit!</p>
+            <p className="landing-page__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure odio sit, quod iusto ducimus libero reprehenderit nulla dignissimos necessitatibus quisquam esse minima sint aspernatur modi tenetur voluptate pariatur illo suscipit!</p>
+            <p className="landing-page__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure odio sit, quod iusto ducimus libero reprehenderit nulla dignissimos necessitatibus quisquam esse minima sint aspernatur modi tenetur voluptate pariatur illo suscipit!</p>
+            {/* <div className="landing-page__ship-container">
+            <img className="landing-page__pirate-ship" src="src/assets/pirate-ship-and-sea.png" alt="pirate-ship-on-the-sea" />
+            <img className="landing-page__sea" src="src/assets/sea-background.png" alt="pirate-ship-on-the-sea" />
+            </div> */}
+        </div>
     )
 }
 
