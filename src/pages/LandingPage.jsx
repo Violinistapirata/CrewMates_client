0bd2899215ebe7f94cd178853dc93b53f2b3466c
Eleni Kriezia <173841123+nthTimeIsTheCharm@users.noreply.github.com>
2025-03-07 18:48:12 +0100

    Solved mergin conflict

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,7 +3,6 @@ import shipWithWater from "../assets/pirate-ship-and-sea-short.png";
 
 function LandingPage() {
   return (
-<<<<<<< HEAD
     <div className="LandingPage__container">
       <div className="LandingPage__title-with-subtitle">
         <h1 className="LandingPage___title">Welcome to Crewmates!</h1>
@@ -62,27 +61,6 @@ function LandingPage() {
           </p>
         </div>
       </div>
-=======
-    <div className="landing-page-container">
-      <h2 className="landing-page-title">Welcome to Crewmates!</h2>
-      <p className="landing-page-text">
-        "Ahoy, matey! Ready to join CrewMates, the ultimate treasure for taming
-        household chores? Arrr, no need to walk the plank here! With CrewMates,
-        signing up is quicker than a parrot swiping doubloons 🦜💰. Assemble yer
-        crew with those scallywags sharing yer deck (be they family or
-        flatmates) and mark 'em like treasures on yer map! Assign chores like a
-        fair captain! No one'll escape scrubbin' dishes 🍽️⚔️ or wringin' rags...
-        or they'll face the Responsibility Kraken! Organize tasks with lists
-        shinier than a chest overflowing with doubloons, and make sure every
-        scallywag pulls their weight... or ye'll make 'em walk the plank
-        (metaphorically, aye)! With CrewMates, chores become a crew feast, not a
-        mutiny! Yarrr, cleaning's never been this fun... or at least less
-        hateful! 🏴☠️✨ Weigh anchor and sail toward a chaos-free home, or I'll
-        make ye dance the hornpipe! Beware! If ye neglect it, dust'll pile
-        high... and CrewMate's guardian parrot will come for yer dirty socks.
-        🦜🧦🔥"
-      </p>
->>>>>>> about-page
     </div>
   );
 }
